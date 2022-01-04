08d1e2dbb0665d5eed4eafe9d90e57e73181dad7
Florian Mötz <fm@tailored-apps.com>
2022-01-04 20:07:41 +0100

    Fix input field widths

--- src/main/kotlin/dev/moetz/buzzer/template/JoinLobbyTemplate.kt
+++ src/main/kotlin/dev/moetz/buzzer/template/JoinLobbyTemplate.kt
@@ -10,14 +10,15 @@ class JoinLobbyTemplate(
         if (lobbyCode == null) {
             div(classes = "centered") {
                 form(action = "/join", method = FormMethod.post) {
+                    br()
                     span(classes = "centered-text") { +"Join a Lobby:" }
                     br()
-                    textInput(name = "lobbyCode", classes = "form-input") {
+                    textInput(name = "lobbyCode", classes = "centered form-input") {
                         required = true
                         pattern = "[a-zA-Z0-9-_]+"
                         placeholder = "Lobby-Code"
                     }
-                    textInput(name = "nickname", classes = "form-input") {
+                    textInput(name = "nickname", classes = "form-input centered") {
                         required = true
                         placeholder = "Nickname"
                         pattern = "[a-zA-Z0-9-_\\s]+"
@@ -33,7 +34,7 @@ class JoinLobbyTemplate(
                 form(action = "/join", method = FormMethod.post) {
                     +"Enter a nickname to join the lobby:"
                     br()
-                    textInput(name = "nickname", classes = "form-input") {
+                    textInput(name = "nickname", classes = "form-input centered") {
                         required = true
                         placeholder = "Nickname"
                         pattern = "[a-zA-Z0-9-_\\s]+"
